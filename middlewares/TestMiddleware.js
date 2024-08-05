f32f65ccb3e1e2c724081de33199a2869b5ecbf7
diarietoudiop <diarietou07diop@gmail.com>
2024-08-05 20:38:35 +0300

    changement testte 3 du middleware

--- middlewares/TestMiddleware.js
+++ middlewares/TestMiddleware.js
@@ -16,7 +16,7 @@ export default class Middleware {
             res.status(401).json({ error: 'Access denied, token is invalid' });
         }
     }
-    static test2=(req,res,yokhl)=>{
+    static test3=(req,res,yokhl)=>{
         try{
             console.log("yokh2");
             // const token = req.headers.authorization.split(' ')[1];
