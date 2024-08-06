30e431df2eff1ca249f84ee6d5de87b5ab8f9f42
diopsysteme <102479876+diopsysteme@users.noreply.github.com>
2024-08-06 19:16:50 +0000

    commit (#8)
    Co-authored-by: sidy Diop Balde <sididiop53@gmail.com>

--- do.js
+++ do.js
@@ -13,15 +13,15 @@
     
 =======
 
-    "mail":"2s@gmail.com",
-    "password":"passer",
-    "nom":"sss",
-    "telephone":233443333,
-    "type":"tailleur",
-    "passconfirm":"passer",
-    "prenom":"dio"
-    "type":"tailleur"
-}
+//     "mail":"2s@gmail.com",
+//     "password":"passer",
+//     "nom":"sss",
+//     "telephone":233443333,
+//     "type":"tailleur",
+//     "passconfirm":"passer",
+//     "prenom":"dio"
+//     "type":"tailleur"
+// }
 {
 
 
