1e424f06f807ed99fadaf9c5ab121e305d079428
sidy Diop Balde <sididiop53@gmail.com>
2024-08-06 19:08:04 +0000

    commit

--- do.js
+++ do.js
@@ -1,15 +1,15 @@
-{
+// {
     
 
-    "mail":"2s@gmail.com",
-    "password":"passer",
-    "nom":"sss",
-    "telephone":233443333,
-    "type":"tailleur",
-    "passconfirm":"passer",
-    "prenom":"dio"
-    "type":"tailleur"
-}
+//     "mail":"2s@gmail.com",
+//     "password":"passer",
+//     "nom":"sss",
+//     "telephone":233443333,
+//     "type":"tailleur",
+//     "passconfirm":"passer",
+//     "prenom":"dio"
+//     "type":"tailleur"
+// }
 {
 
 
