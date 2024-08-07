433dbf9f001c2c4c44be4da5cbc16d6ff36271ad
palaye08 <adioptp9@gmail.com>
2024-08-07 15:58:30 +0000

    oo

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,4 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
 const postSchema = new Schema({
-<<<<<<< Updated upstream
   userId: { type: Schema.Types.ObjectId, ref: 'User' },
   contenu: String,
   contenuMedia:[
@@ -23,15 +19,6 @@ const postSchema = new Schema({
     }
   ],
   dislikes: [
-=======
-<<<<<<< Updated upstream
-    name: String,
-    description: String,
-    price: Number,
-    quantity: Number,
-    userId: { type: Schema.Types.ObjectId, ref: 'User' },
-    comments: [
->>>>>>> Stashed changes
       {
         idLikerD: { type: Schema.Types.ObjectId, ref: 'User' }
       }
@@ -41,7 +28,6 @@ const postSchema = new Schema({
         type: String,
         idLikerD: { type: Schema.Types.ObjectId, ref: 'User' }
       }
-<<<<<<< Updated upstream
     ],
   signale: [
       {
@@ -50,57 +36,9 @@ const postSchema = new Schema({
       }
     ],
 
-=======
-    ]
-  });
-  
-  const Post = mongoose.model('Post', postSchema);
-  export default Post;
-  
-=======
-  userId: { type: Schema.Types.ObjectId, ref: 'User' },
-  contenu: { type: String },
-  contenuMedia: [
-    { url: { type: String } }
-  ],
-  comments: [
-    {
-      text: { type: String },
-      createdAt: { type: Date, default: Date.now },
-      updatedAt: { type: Date, default: Date.now },
-      commenterId: { type: Schema.Types.ObjectId, ref: 'User' }
-    }
-  ],
-  likes: [
-    {
-      idLikerD: { type: Schema.Types.ObjectId, ref: 'User' }
-    }
-  ],
-  dislikes: [
-    {
-      idLikerD: { type: Schema.Types.ObjectId, ref: 'User' }
-    }
-  ],
-  likesDislikes: [
-    {
-      type: { type: String },
-      idLikerD: { type: Schema.Types.ObjectId, ref: 'User' }
-    }
-  ],
-  signale: [
-    {
-      motif: { type: String },
-      idLikerD: { type: Schema.Types.ObjectId, ref: 'User' }
-    }
-  ],
->>>>>>> Stashed changes
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
 const Post = mongoose.model('Post', postSchema);
 export default Post;
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
