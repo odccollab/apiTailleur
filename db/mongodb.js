eed16b29e227781bbbe926626f3c5009faa44bfe
diarietoudiop <diarietou07diop@gmail.com>
2024-08-08 15:54:39 +0000

    diarry merge conflits

--- db/mongodb.js
+++ db/mongodb.js
@@ -8,14 +8,9 @@ class Database {
   }
 
   _connect() {
-<<<<<<< HEAD
     mongoose.connect(process.env.MONGO_URL1)
       .then(() => {
-=======
-    mongoose.connect(process.env.MONGO_URL)
-      .then(() => {
-        dbName:"apiTailleur"
->>>>>>> bf354c3 (fonctionnalité message et partage sur la nouvelle branche)
+
         console.log('Database connection successful');
       })
       .catch(err => {
@@ -25,25 +20,3 @@ class Database {
 }
 
 export default new Database();
-<<<<<<< HEAD
-=======
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URL, {
-
-//             dbName: "apiTailleur"  // 
-
-//         });
-
-//         console.log('MongoDB connected');
-//     } catch (error) {
-//         console.error('Error connecting to MongoDB:', error.message);
-//     }
-// };
-
-// export default connectDB;
->>>>>>> bf354c3 (fonctionnalité message et partage sur la nouvelle branche)
