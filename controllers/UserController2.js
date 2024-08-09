3aca1be59dcac39f218c741f94ea889888f50a2b
diopsysteme <102479876+diopsysteme@users.noreply.github.com>
2024-08-09 19:25:59 +0000

    Update UserController2.js

--- controllers/UserController2.js
+++ controllers/UserController2.js
@@ -35,84 +35,8 @@ export default class UserController2 {
             res.status(500).send('Erreur du serveur');
         }
     }
-      static async manageFavorites(req, res) {
-    const { postId } = req.body;
-    const userId = req.id; // Récupération de l'ID de l'utilisateur depuis le middleware d'authentification
 
-    try {
-      // Vérifier l'existence de l'utilisateur
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(400).send('Vous n\'êtes pas connecté');
-      }
-
-      // Vérifier l'existence du post
-      const post = await Post.findById(postId);
-      if (!post) {
-        return res.status(404).send('Le post n\'existe pas');
-      }
-
-      // Vérifier si le post est déjà dans les favoris de l'utilisateur
-      const postIndex = user.favoris.indexOf(postId);
-
-      if (postIndex === -1) {
-        // Le post n'est pas dans les favoris, donc on l'ajoute
-        user.favoris.push(postId);
-        await user.save();
-        return res.json({ message: "Post ajouté aux favoris", data: user });
-      } else {
-        // Le post est déjà dans les favoris, donc on le supprime
-        user.favoris.splice(postIndex, 1);
-        await user.save();
-        return res.json({ message: "Post retiré des favoris", data: user });
-      }
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Erreur du serveur');
-    }
-  }
-
-    //---------------------------------------Affichage Favoris-----------------------------------------
-    static async getUserFavorites(req, res) {
-      const userId = req.id; // Récupération de l'ID de l'utilisateur depuis le middleware d'authentification
-    
-      try {
-        // Trouver l'utilisateur et ses favoris, en incluant les informations de l'utilisateur qui a publié chaque post favori
-        const user = await User.findById(userId)
-          .populate({
-            path: 'favoris',
-            populate: {
-              path: 'userId',
-              select: 'nom prenom'
-            }
-          });
-    
-        if (!user) {
-          return res.status(400).send('Vous n\'êtes pas connecté');
-        }
-    }
-   
   
-
-    if (password !== passconfirm) {
-      return res.status(400).send('Les mots de passe ne correspondent pas');
-    }
-    password = Utils.hashPassword(password);
-    try {
-      const user = await User.create({
-        nom,
-        prenom,
-        role,
-        password,
-        telephone,
-        mail,
-      });
-      res.json(user);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Erreur du serveur');
-    }
-  }
 
   //---------------------------------------favoris-----------------------------------------
   static async manageFavorites(req, res) {
