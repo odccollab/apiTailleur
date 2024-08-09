a101781c61f2dec4ee4f2bd64430662497f21089
maamdaam <damewade20@gmail.com>
2024-08-09 13:16:19 +0000

    commit (#8)
    Co-authored-by: sidy Diop Balde <sididiop53@gmail.com>

--- do.js
+++ do.js
@@ -1,7 +1,6 @@
 // {
     
 
-<<<<<<< HEAD
 //     "mail":"2s@gmail.com",
 //     "password":"passer",
 //     "nom":"sss",
@@ -13,26 +12,14 @@
 // {
 
 
-=======
->>>>>>> f05d575 (sidi push)
-    "mail":"2s@gmail.com",
-    "password":"passer",
-    "nom":"sss",
-    "telephone":233443333,
-    "type":"tailleur",
-    "passconfirm":"passer",
-    "prenom":"dio"
-    "type":"tailleur"
-}
+
 {
 
 
 //     "mail":"sss@gmail.com",
 //     "password":"passer"
-<<<<<<< HEAD
     
-=======
->>>>>>> f05d575 (sidi push)
+
     
 //     "mail":"sss@gmail.com",
 //     "password":"passer"
