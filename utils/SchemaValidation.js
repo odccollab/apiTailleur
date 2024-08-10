75882c76e28d6b92f1a5a6156233570aba567236
diopsysteme <102479876+diopsysteme@users.noreply.github.com>
2024-08-10 19:15:38 +0000

    Update SchemaValidation.js

--- utils/SchemaValidation.js
+++ utils/SchemaValidation.js
@@ -44,40 +44,46 @@ const validationSchemas = {
         "string.pattern.base":
           "L'image doit être un fichier avec une extension valide (jpg, jpeg, png, gif)",
       }),
-       login : Joi.object({
-         
-          mail: Joi.string().required().email().messages({
-            'string.base': 'Le prénom doit être une chaîne de caractères',
-            'string.empty': 'Le prénom ne doit pas être vide',
-            'string.email': 'L\'email doit être valide'
-          }),
-          password: Joi.string().required().messages({
-            'string.base': 'Le mot de passe doit être une chaîne de caractères',
-            'string.empty': 'Le mot de passe ne doit pas être vide'
-          }),
-         
-        }),
-        post: Joi.object({
-          contenu: Joi.string().required().messages({
-            'string.base': 'Le contenu doit être une chaîne de caractères',
-            'string.empty': 'Le contenu est obligatoire',
-            'any.required': 'Le champ contenu est requis'
-          }),
-          contenuMedia: Joi.array().items(Joi.string().uri().regex(/\.(jpeg|jpg|gif|png|mp4|mov)$/)).optional().messages({
-            'array.base': 'Le contenu média doit être un tableau',
-            'array.includes': 'Le contenu média doit contenir des URLs valides de vidéos ou d\'images'
-          }).default([])  // Default to an empty array if not provided
-        }),
-        comment: Joi.object({
-          text: Joi.string().optional().messages({
-            'string.base': 'Le texte du commentaire doit être une chaîne de caractères',
-            'string.empty': 'Le texte du commentaire ne doit pas être vide',
-            'any.required': 'Le champ de texte du commentaire est requis'
-          })
+    }),
+    login: Joi.object({
+      mail: Joi.string().required().email().messages({
+        "string.base": "Le prénom doit être une chaîne de caractères",
+        "string.empty": "Le prénom ne doit pas être vide",
+        "string.email": "L'email doit être valide",
+      }),
+      password: Joi.string().required().messages({
+        "string.base": "Le mot de passe doit être une chaîne de caractères",
+        "string.empty": "Le mot de passe ne doit pas être vide",
+      }),
+    }),
+    post: Joi.object({
+      contenu: Joi.string().required().messages({
+        "string.base": "Le contenu doit être une chaîne de caractères",
+        "string.empty": "Le contenu est obligatoire",
+        "any.required": "Le champ contenu est requis",
+      }),
+      contenuMedia: Joi.array()
+        .items(
+          Joi.string()
+            .uri()
+            .regex(/\.(jpeg|jpg|gif|png|mp4|mov)$/)
+        )
+        .optional()
+        .messages({
+          "array.base": "Le contenu média doit être un tableau",
+          "array.includes":
+            "Le contenu média doit contenir des URLs valides de vidéos ou d'images",
         })
-
-  }),
+        .default([]), // Default to an empty array if not provided
+    }),
+    comment: Joi.object({
+      text: Joi.string().optional().messages({
+        "string.base":
+          "Le texte du commentaire doit être une chaîne de caractères",
+        "string.empty": "Le texte du commentaire ne doit pas être vide",
+        "any.required": "Le champ de texte du commentaire est requis",
+      }),
+    }),
   
-
 };
 export default validationSchemas;
