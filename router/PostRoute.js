96e1dbbb0fe491d30167026778e7e2a6e943901c
diopsysteme <102479876+diopsysteme@users.noreply.github.com>
2024-08-12 22:44:04 +0000

    Update PostRoute.js

--- router/PostRoute.js
+++ router/PostRoute.js
@@ -12,47 +12,52 @@ import CanPost from '../middlewares/CanPostMiddleware.js';
 
 
 const router = express.Router();
-
-// router.get('/:id', PostController.getUserPosts);
-// router.get('/:userId/:postId', PostController.getOneUserPost);
-// router.get('/comment/:userId/:postId', PostController.getComment);
-// router.get('/like/:userId/:postId', PostController.getLike);
-// router.get('/numberlike/:userId/:postId', PostController.getNumberLike);
-
-router.get('/status', PostController.getAllStatus);
-
-// Nouvelles routes pour les commentaires
-router.post('/:postId/comment',verifyToken(), validateData('comment'), postController.addComment);
-router.get('/:postId/comment', postController.getComments); 
-
-
-
-// Nouvelles routes pour les vues
-// router.post('/:postId/view', ViewController.incrementViews);
-router.get('/:postId/view',verifyToken(), postController.incrementViews);
-router.get('/:postId/views',verifyToken(), PostController.getViews);
-router.get('/create', validateData("post"), PostController.createPost);
-
-router.delete('/:postId/comment/:commentId', PostController.deleteComment);
-router.put('/:postId/comment/:commentId',validateData('comment'), PostController.updateComment);
-
-
-
+// creer un post les variable requis { contenu, contenuMedia }
 
 router.post('/create',MiddlewareToken(),Validator("post"), CanPost(),PostController.createPost);
-router.post('/createStory',MiddlewareToken(),Validator("post"),CanPost(), PostController.createStory);
-router.get('/:type/:idpost',MiddlewareToken(), PostController.handleLikeDislike);
-// router.get('/postsc',MiddlewareToken(), PostController.allPost);
-// router.get('/posts', PostController.allPost);
+//modifier un post les variable requis { contenu, contenuMedia }
 router.put('/:id',MiddlewareToken(),Validator("post"), PostController.modifyPost);
+//recuperer tous les status 
+router.get('/status', PostController.getAllStatus);
+//delete a post
 router.delete('/:id',MiddlewareToken(), PostController.deletePost);
+//commentaire
+//ajouter un commentaire les variable requis 
+// const { postId, commentId } = req.params;
+//     const { text } = req.body;
+router.post('/:postId/comment', verifyToken(), validateData('comment'), postController.addComment);
+//recuperer tous les commentaires d'un post 
+router.get('/:postId/comment', postController.getComments);
+//modifier un commentaire  
+// const { postId, commentId } = req.params;
+//     const { text } = req.body;
+router.put('/:postId/comment/:commentId',validateData('comment'), PostController.updateComment);
+//supprimer un commentaire 
+router.delete('/:postId/comment/:commentId', PostController.deleteComment);
+//story 
+router.post('/createStory',MiddlewareToken(),Validator("post"),CanPost(), PostController.createStory);
+
+
+// Nouvelles routes pour les vues 
+router.get('/:postId/view',verifyToken(), postController.incrementViews);
+//voilr les vue d'un post 
+router.get('/:postId/views',verifyToken(), PostController.getViews);
+//file actu 
 router.get('/accueil',MiddlewareToken(),PostController.fileActu)
 
-router.post('/create',MiddlewareToken(),Validator("post"), PostController.createPost);
+
+//liker dislike
+router.get('/:type/:idpost',MiddlewareToken(), PostController.handleLikeDislike);
+
+//partage post a un user de l'appli
 router.post('/share', PostController.sharePost);
+//partage post a un user via email, facebook, whatsapp
 router.post('/share/email', PostController.shareByEmail);
 router.post('/share/facebook', PostController.shareOnFacebook);
 router.post('/share/whatsapp', PostController.shareOnWhatsApp);
+//signale un use
  router.post('/signale',MiddlewareToken(), PostController.signalPost);
+ //recherche utilisateur ou post ça prend  {value}
  router.get('/find', PostController.findUserOrPost);
+
 export default router;
