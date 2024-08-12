74017c8243c657165d5dccc7261fc493f11fa3b1
diopsysteme <102479876+diopsysteme@users.noreply.github.com>
2024-08-12 22:43:38 +0000

    Update UserRoute.js

--- router/UserRoute.js
+++ router/UserRoute.js
@@ -6,40 +6,36 @@ import Validator from '../middlewares/ValidatorMiddleware.js';
 import MiddlewareToken from '../middlewares/TokenMiddleware.js';
 const router = express.Router();
 
-
-router.post('/users', UserController.createUser);
-router.get('/email/:email',UserController.findUserByEmail);
-router.get('/email',Middleware.test,Middleware.whoru, UserController.findUserByEmail2);
+//create User ça prend { nom, prenom, role, password, telephone, mail,passconfirm }
+router.post('/create',Validator("register"),  UserController2.createUser);
+//avoir profile du user connected
+router.get('/profile',Middleware.test,Middleware.whoru, UserController.profile);
+//login user ça prend { mail, password }
 router.post('/login2',Validator("login"), UserController.loginUser);
- router.post('/login',Middleware.Validate,UserController.loginUser);
+ //ajouter ou enlever un follower pour un user ça prend  {  followedId }
 router.post('/follow',MiddlewareToken(), UserController.addFollower);
+//lister les followers du user connecté
 router.get('/followers', MiddlewareToken(),UserController.getFollowers);
+//lister les utilisateurs qui sont followés par le user connecté
 router.get('/followings',MiddlewareToken(),UserController.getFollowings);
- router.post('/create',Validator("register"),  UserController2.createUser);
- router.post('/achatCredit', MiddlewareToken(), UserController.rechargerCompte);
- router.post('/modifyProfile', MiddlewareToken(), UserController.ChangeEnTailleur)
+//achat credit  ça prend  {  amount }
+router.post('/achatCredit', MiddlewareToken(), UserController.rechargerCompte);
+//changer la tailleur  ça prend  rien 
+router.post('/modifyProfile', MiddlewareToken(), UserController.ChangeEnTailleur)
+//ajouter ou enlever favori ça prend { postId }
 router.post('/favorite',MiddlewareToken(), UserController2.manageFavorites);
+//lister les favoris du user connecté
 router.get('/favorite',MiddlewareToken(), UserController2.getUserFavorites);
+//ajouter ou enlever vote pour un post ça prend  {  voteForUserId }
 router.post('/vote',MiddlewareToken(), UserController2.manageVotes);
-
+//avoir les messages pour le user connecte
 router.get('/messages', Middleware.test, UserController.getMessages);
-// router.get('/messages/:userId', Middleware.test, UserController.getMessagesByUserId);
+//envoyer message  ça prend { receiver, content } 
 router.post('/messages', Middleware.test, UserController.sendMessage);
+//rechercher message  ça prend { searchText, startDate, endDate, senderId }
 router.get('/messages/search', Middleware.test, UserController.searchMessages);
+// router.get('/messages/:userId', Middleware.test, UserController.getMessagesByUserId);
 
-// Vous pouvez ajouter d'autres routes ici
-// router.get('/', UserController.getAllPosts);
-// router.post('/login', UserController.login);
-// router.post('/register', UserController.register);
-// router.get('/profile', Middleware.test,UserController.getUser2);
-// router.get('/:id/profile',Middleware.test,Middleware.test2, UserController.getUser);
-// router.get('/:id/posts', UserController.getPosts);
-// router.get('/:id/votes', UserController.getVotes);
-// router.get('/:id/followers', UserController.getFollowers);
-// router.get('/:id/following', UserController.getFollowing);
-// router.post('/:userId/posts/:postId/comment', UserController.makeComment);
-// router.post('/:userId/posts/:postId/like', UserController.makeLike);
-// router.post('/:userId/follow/:followerId', UserController.followUser);
-// router.delete('/:userId/unfollow/:followerId', UserController.unfollowUser);
+
 
 export default router;      
