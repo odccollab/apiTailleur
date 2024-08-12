3f6b4b15b55344437065af3f5867a445b3f23982
diopsysteme <102479876+diopsysteme@users.noreply.github.com>
2024-08-12 23:28:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ import connectDB from './db/mongodb.js';
 import postRoute from './router/PostRoute.js';
 import userRoute from './router/UserRoute.js';
 import dotenv from 'dotenv';
-import swaggerSetup from './swagger.js';
+import './middlewares/cront.js';
+import swaggerSetup from './swagger.js'
+
 import http from 'http';
+import userRoutes from './router/UserRoute.js';
+// import messageRoutes from './router/MessageRoute.js';
+// import setupSocket from './socket/socket.js';
 import { Server } from 'socket.io';
 
 dotenv.config();
-
 const app = express();
-swaggerSetup(app);
+swaggerSetup(app)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+connectDB;
 
-// Connect to MongoDB
-connectDB();
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -24,12 +27,13 @@ const io = new Server(server, {
     origin: '*',
   }
 });
-
 app.use(express.json());
-app.use("/posts", postRoute);
-app.use("/users", userRoute);
+app.use("/posts",postRoute);
+app.use("/users",userRoute);
+
 
 const PORT = process.env.PORT || 3000;
-server.listen(80, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
